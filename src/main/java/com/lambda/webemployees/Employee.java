c11cfbb5c920d1e001e7db4a44204289e5a023b0
dislersd <dislersd@gmail.com>
2020-07-07 20:02:09 -0700

    initial commit

---
+++ src/main/java/com/lambda/webemployees/Employee.java
@@ -0,0 +1,11 @@
+package com.lambda.webemployees;
+
+public class Employee {
+    private String fname;
+    private String lname;
+    private double salary;
+    private boolean has401k;
+    private long companyId;
+    private long healthplanId;
+
+}
