ede2af9abc1e4433de70dfaea7c565cd45e543cb
dislersd <dislersd@gmail.com>
2020-07-08 19:56:57 -0700

    created route to get single employee by id

--- src/main/java/com/lambda/webemployees/EmployeeController.java
+++ src/main/java/com/lambda/webemployees/EmployeeController.java
@@ -3,6 +3,7 @@ package com.lambda.webemployees;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,11 @@ public class EmployeeController {
     public ResponseEntity<?> getAllEmployees() {
         WebEmployeesApplication.ourEmpList.empList.sort((e1, e2) -> (int) (e1.getSalary() - e2.getSalary()));
         return new ResponseEntity<>(WebEmployeesApplication.ourEmpList.empList, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/employee/{empid}", produces = {"application/json"})
+    public ResponseEntity<?> getEmployeeById(@PathVariable long empid) {
+        Employee rtnEmp = WebEmployeesApplication.ourEmpList.findEmployee(e -> (e.getId() == empid));
+        return new ResponseEntity<>(rtnEmp, HttpStatus.OK);
     }
 }
