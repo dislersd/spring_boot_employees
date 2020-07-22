c08fc3321f18d88b56c203a737f3b5e51f775182
dislersd <dislersd@gmail.com>
2020-07-21 20:12:22 -0700

    setting up Employee table with Java class

---
+++ src/main/java/com/lambda/webemployees/models/Employee.java
@@ -0,0 +1,63 @@
+package com.lambda.webemployees.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Employee {
+    long empid;
+    String fname;
+    String lname;
+    double salary;
+
+    public Employee(String fname, String lname, double salary) {
+        this.fname = fname;
+        this.lname = lname;
+        this.salary = salary;
+    }
+
+    public Employee() {
+        // default constructor is required by Spring Data JPA
+    }
+
+    public long getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(long empid) {
+        this.empid = empid;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empid=" + empid +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
